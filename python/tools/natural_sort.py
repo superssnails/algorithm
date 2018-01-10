000c01cab152a22a77092746bf6e04f6ef36306f
zhangran@acerpc <zhangran@advancesyn.com>
2018-01-11 00:38:11 +0800

    + python natural sort code without lambda

---
+++ python/tools/natural_sort.py
@@ -0,0 +1,25 @@
+import re
+
+
+def convert(text):
+    if text.isdigit():
+        return int(text)
+    else:
+        return text.lower()
+
+
+def alphanum_key(element):
+
+    return_data= []
+    for c in re.split('([0-9]+)', element):
+        converted_value = convert(c)
+        return_data.append(converted_value)
+
+    return return_data
+
+
+def natural_sort(data):
+
+    return sorted(data, key = alphanum_key)
+
+
